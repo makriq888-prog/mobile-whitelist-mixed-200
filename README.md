efd16577933ce993df36c88ef48ef003a3d7f98d
makriq2 <makriq888@gmail.com>
2026-04-04 14:53:51 +0300

    Remove WHITE-CIDR-RU-all upstream

--- README.md
+++ README.md
@@ -31,7 +31,6 @@
 - `https://raw.githubusercontent.com/zieng2/wl/main/vless_lite.txt`
 - `https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile.txt`
 - `https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile-2.txt`
-- `https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-all.txt`
 
 ## Структура
 
--- scripts/check_key_list.py
+++ scripts/check_key_list.py
@@ -24,7 +24,6 @@ DEFAULT_SOURCES = [
     "https://raw.githubusercontent.com/zieng2/wl/main/vless_lite.txt",
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile.txt",
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile-2.txt",
-    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-all.txt",
 ]
 DEFAULT_TEST_URL = "https://www.gstatic.com/generate_204"
 DEFAULT_OUTPUT = Path("artifacts/short-key-list.txt")
